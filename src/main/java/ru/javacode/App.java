0d67727bd982baa21d4ee3d03b82855f05b35b1e
unknown <enzzomatrix@gmail.com>
2024-10-23 17:48:05 +1100

    upd

--- src/main/java/ru/javacode/App.java
+++ src/main/java/ru/javacode/App.java
@@ -25,9 +25,14 @@ public class App {
                 )
             );
 
-        List<Order> sortedOrders = orders.stream()
-                        .sorted((l, r) -> l.getCost() > r.getCost())
+        List<Order> sortedOrders = groupedByOrder.stream()
+            .sorted((l, r) -> Double.compare(r.getCost(), l.getCost()))
+            .limit(2)
+            .collect(Collectors.toList());
 
-        System.out.println("Grouped by product: " + groupedByOrder);
+        System.out.println("Grouped by product: ");
+        groupedByOrder.forEach((product, cost) -> System.out.println(product + ": " + cost));
+        System.out.println("Sorted by price: ");
+        sortedOrders.forEach((order) -> System.out.println(order.getProduct()));
     }
 }
